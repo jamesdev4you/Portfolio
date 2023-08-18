b62bc9bb9f4d054807bc19b04d51f43fd541a7d7
James <jamesdev4you@gmail.com>
2023-08-18 11:54:34 -0400

    added resume pdf

--- src/PrimaryComponents/Footer.js
+++ src/PrimaryComponents/Footer.js
@@ -1,53 +1,67 @@
-import React from "react";
-import Box from "@mui/material/Box";
-import git from "../Images/footergit.png";
-import linkedin from "../Images/footerlinkedin.png";
-import instagram from "../Images/instagram.png";
-import { Typography } from "@mui/material";
+import React from 'react';
+import Box from '@mui/material/Box';
+import git from '../Images/footergit.png';
+import linkedin from '../Images/footerlinkedin.png';
+import instagram from '../Images/instagram.png';
+import resume from '../Documents/Resume.pdf';
+import resumelogo from '../Images/resumelogo.png';
+import { Typography } from '@mui/material';
 
 const Footer = () => {
   return (
     <Box
       sx={{
-        width: "100%",
-        height: "150px",
-        backgroundColor: "black",
-        display: "flex",
-        alignItems: "center",
-        justifyContent: "space-around",
+        width: '100%',
+        height: '150px',
+        backgroundColor: 'black',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'space-around',
       }}
     >
-      <Box sx={{ display: "flex", gap: "50px" }}>
+      <Box sx={{ display: 'flex', gap: '50px' }}>
         <a
-          href="https://www.linkedin.com/in/james-boyle-033ab7276/"
-          target="_blank"
+          href='https://www.instagram.com/jamesboyle813/'
+          target='_blank'
+          rel='noreferrer'
+        >
+          <img src={instagram} style={{ width: 100, height: 100 }} />
+        </a>
+        <a
+          href='https://www.linkedin.com/in/james-boyle-033ab7276/'
+          target='_blank'
+          rel='noreferrer'
         >
           <img src={linkedin} style={{ width: 100, height: 100 }} />
         </a>
-        <a href="https://www.instagram.com/jamesboyle813/" target="_blank">
-          <img src={instagram} style={{ width: 100, height: 100 }} />
-        </a>
-        <a href="https://github.com/jamesdev4you" target="_blank">
+        <a
+          href='https://github.com/jamesdev4you'
+          target='_blank'
+          rel='noreferrer'
+        >
           <img src={git} style={{ width: 100, height: 100 }} />
+        </a>
+        <a href={resume} target='_blank' rel='noreferrer'>
+          <img src={resumelogo} style={{ width: 100, height: 100 }} />
         </a>
       </Box>
       <Box
         sx={{
           display: {
-            xl: "flex",
-            lg: "flex",
-            md: "flex",
-            sm: "none",
-            xs: "none",
+            xl: 'flex',
+            lg: 'flex',
+            md: 'flex',
+            sm: 'none',
+            xs: 'none',
           },
         }}
       >
         <Typography
-          variant="h7"
+          variant='h7'
           sx={{
-            margin: "auto",
-            textAlign: "center",
-            fontFamily: "Ubuntu",
+            margin: 'auto',
+            textAlign: 'center',
+            fontFamily: 'Ubuntu',
           }}
         >
           Made by Jamesdev4you@gmail.com
